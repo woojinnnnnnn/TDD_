48a7d255f3efa19181f22497e493986a612fbabf
song_woojin <ujins8201@gmail.com>
2024-06-03 20:33:58 +0900

    TDD 2st

---
+++ bin/www.js
@@ -0,0 +1,5 @@
+const app = require('../index.js')
+
+app.listen(3000, function () {
+      console.log("Server is listening on port 3000!");
+    });
--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-let user = require ('./api/user/index.js')
+let user = require("./api/user/index.js");
 const app = express();
 
-app.use(morgan("dev"));
+if (process.env.NODE_ENV !== "test") {
+  app.use(morgan("dev"));
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/users', user)
-
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
-});
+app.use("/users", user);
 
 module.exports = app;
